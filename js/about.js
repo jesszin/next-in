b2f934cb0e30542936c49ba9c5eb0bbc4dc25b7f
jinjess <jinareum.76@gmail.com>
2018-03-28 21:40:16 +0900

    edit

--- js/about.js
+++ js/about.js
@@ -1,12 +1,18 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        var scrollTop = $(window).scrollTop();
-        if(scrollTop > 1){
-            $('.aboutWrap').fadeIn(300);
-        }
-        if(scrollTop > 15){
-            $('.missionWrap').fadeIn(300);
-        }
-
-    })
-})
+$(document).ready(function() {
+    $(window).scroll( function(){
+        $('.hide-title').each( function(i){
+            var pagetitle = $(window).scrollTop();
+            if( pagetitle > 0 ){
+                $(this).animate({'opacity':'1'},300);
+            }
+        });
+        $('.hide').each( function(i){
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            if( bottom_of_window > bottom_of_object ){
+                $(this).animate({'opacity':'1'},1000);
+            }
+        });
+    });
+});
+//title

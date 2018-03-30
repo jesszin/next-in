2b41320bde483c1c514fce5294653b1b792fdea2
jinjess <jinareum.76@gmail.com>
2018-03-30 16:15:12 +0900

    edit

--- js/ir02.js
+++ js/ir02.js
@@ -1,30 +1,30 @@
-var svgWidth=350;
-var svgHeight=300;
+var svgWidth=380;
+var svgHeight=340;
 var offsetX=30;
 var offsetY=20;
 var barElements;
-var dataSet=[120,170,220,90];
+var dataSet=[110,80,210,160];
 var dataMax=300;
-var barWidth=50;
-var barMargin=20;
-
+var barWidth=60;
+var barMargin=30;
+//그래프그리기
 barElements=d3.select("#irGraph02")
     .selectAll("rect")
     .data(dataSet)
-
+//데이터추가
 barElements.enter()
     .append("rect")
     .attr("class","bar")
     .attr("height",0)
     .attr("width",barWidth)
     .attr("x",function(d,i){
-        return i * (barWidth+barMargin)+offsetX;
+        return i*(barWidth+barMargin)+offsetX;
     })
     .attr("y",svgHeight-offsetY)
     .transition()
     .duration(1000)
     .delay(function(d,i){
-        return i*100;
+        return i*150;
     })
     .attr("y",function(d,i){
         return svgHeight-d-offsetY;
@@ -32,35 +32,30 @@ barElements.enter()
     .attr("height",function(d,i){
         return d;
     })
-
 barElements.enter()
     .append("text")
     .attr("class","barNum")
     .attr("x",function(d,i){
-        return i *(barWidth+barMargin)+30+offsetX;
+        return i*(barWidth+barMargin)+30+offsetX;
     })
-    .attr("y",svgHeight- 25 -offsetY)
+    .attr("y",svgHeight-5-offsetY)
     .text(function(d,i){
         return d;
     })
-
-var yScale = d3.scaleLinear()
+var yScale=d3.scale.linear()
     .domain([0,dataMax])
     .range([dataMax,0])
-
-//세로 눈금
 d3.select("#irGraph02")
     .append("g")
     .attr("class","axis")
     .attr("transform","translate("+offsetX+","+((svgHeight-300)-offsetY)+")")
     .call(
-        d3.axisLeft()
+        d3.svg.axis()
             .scale(yScale)
+            .orient("left")
             .ticks(5)
             .tickValues([50,100,150,200,250])
     )
-
-//가로 선
 d3.select("#irGraph02")
     .append("rect")
     .attr("class","axis_x")
@@ -72,9 +67,9 @@ barElements.enter()
     .append("text")
     .attr("class","barName")
     .attr("x",function(d,i){
-        return i*(barWidth+barMargin)+12+offsetX;
+        return i*(barWidth+barMargin)+30+offsetX;
     })
-    .attr("y",svgHeight-offsetY+15)
+    .attr("y",svgHeight-offsetY+20)
     .text(function(d,i){
         return["1분기","2분기","3분기","4분기"][i];
     })
